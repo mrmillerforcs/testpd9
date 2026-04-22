9018d7468110ab5409c4f5022ea9f8073bd46a9b
BT_C09_33 <mobileappdev@bths.edu>
2026-04-22 14:04:42 -0400

    Initial Commit

---
+++ testpd9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testpd9
+//
+//  Created by Teacher on 4/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testpd9/testpd9App.swift
@@ -0,0 +1,17 @@
+//
+//  testpd9App.swift
+//  testpd9
+//
+//  Created by Teacher on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct testpd9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
